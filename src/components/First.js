f02bb4d05f8d59ae5cf1c38a92b7ce23c306a21f
Krystian Kalwik <ambitious156288@gmail.com>
2021-12-13 22:30:23 +0100

    CRA project setup - ESlint, Prettier, Husky & lint-staged, absolute imports, styled components

---
+++ src/components/First.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const First = () => {
+  return (
+    <>
+      <p>First</p>
+    </>
+  );
+};
+
+export default First;
---
+++ src/views/Root/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from '../../theme/GlobalStyle';
+
+import First from '../../components/First';
+
+const Root = () => (
+  <>
+    <GlobalStyle />
+
+    <First />
+  </>
+);
+
+export default Root;
